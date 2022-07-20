a26428dbd3245a3da79fb858123ff639eea15603
SUNcHeZz <dj_sunchezz@mail.ru>
2022-07-20 17:35:20 +0500

    add GraphDijkstra.py

---
+++ GraphDijkstra.py
@@ -0,0 +1,19 @@
+# Dijkstra graph search algorithm
+
+graph = {'start': {'a': 6, 'b': 2}, 'a': {'fin': 1}, 'b': {'a': 3, 'fin': 5}, 'fin': {}}
+# graph = {}
+# graph['start'] = {}
+# graph['start']['a'] = 6
+# graph['start']['b'] = 2
+# graph['a'] = {}
+# graph['a']['fin'] = 1
+# graph['b'] = {}
+# graph['b']['a'] = 3
+# graph['b']['fin'] = 5
+# graph['fin'] = {}
+infinity = float('inf')  # бесконечность
+costs = {'a': 6, 'b': 2, 'fin': infinity}  # стоимости обновляются в процессе
+parents = {'a': 'start', 'b': 'start', 'fin': None}  # родители узла обновляются в процессе
+
+
+print(graph.keys())
