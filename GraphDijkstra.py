c0e1b83e4225a71d7377d1d8f6d0d2e4ee071462
SUNcHeZz <dj_sunchezz@mail.ru>
2022-07-22 11:02:17 +0500

    final GraphDijkstra.py

--- GraphDijkstra.py
+++ GraphDijkstra.py
@@ -1,6 +1,6 @@
 # Dijkstra graph search algorithm
 
-graph = {'start': {'a': 6, 'b': 2}, 'a': {'fin': 1}, 'b': {'a': 3, 'fin': 5}, 'fin': {}}
+
 # graph = {}
 # graph['start'] = {}
 # graph['start']['a'] = 6
@@ -11,17 +11,46 @@ graph = {'start': {'a': 6, 'b': 2}, 'a': {'fin': 1}, 'b': {'a': 3, 'fin': 5}, 'f
 # graph['b']['a'] = 3
 # graph['b']['fin'] = 5
 # graph['fin'] = {}
+
+graph = {'start': {'a': 6, 'b': 2}, 'a': {'fin': 1}, 'b': {'a': 3, 'fin': 5}, 'fin': {}}
 infinity = float('inf')  # бесконечность
 costs = {'a': 6, 'b': 2, 'fin': infinity}  # стоимости обновляются в процессе
 parents = {'a': 'start', 'b': 'start', 'fin': None}  # родители узла обновляются в процессе
 
-print(graph.keys())
-print(graph.values())
-print(graph.items())
-print(graph.get('start'))
-print(graph.items())
-print(graph.pop('start'))
-print(graph.items())
-print('a' in graph)
 
+def find_lowest_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+
+processed = []
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+print('graph:', graph)
+print('start to fin cost =', costs.get('fin'))
+
+# dictionary commands:
 # print(graph.keys())
+# print(graph.values())
+# print(graph.items())
+# print(graph.get('start'))
+# print(graph.items())
+# print(graph.pop('start'))
+# print(graph.items())
+# print('a' in graph)
